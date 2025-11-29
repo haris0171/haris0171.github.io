e31786811ad928f08c893b2b47b3abc0aa1cf2f7
Haris Hussain <haris_hussain@outlook.com>
2025-11-29 15:17:01 -0500

    fixed error for constants
    As requested from rubric made correct changes to constant names.

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -11,11 +11,10 @@ function randomValueFromArray(array) {
 
 // Raw text strings
 
-const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-
-const places = ["the soup kitchen", "Disneyland", "the White House"];
-
-const events = ["spontaneously combusted", "melted into a puddle on the sidewalk","turned into a slug and slithered away"];
+const storyText =  "It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk","turned into a slug and slithered away"];
 
 
 // Partial return random string function
