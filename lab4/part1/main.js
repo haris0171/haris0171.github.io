d99d59f32d933cdf2957e4a5c640c7a1a1bc3bfa
Haris Hussain <haris_hussain@outlook.com>
2025-12-05 02:40:36 -0500

    Created newStory.replaceAll() function + Minor readability changes
    in the result() function, used a newStory.replaceAll() function call to replace :insertx:, :inserty: and :insertz: with the random insertX, insertY and insertZ variables.
    
    Along with this I did some clean up with the code to improve readability with additional comments on parts of result() function

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -9,21 +9,23 @@ function randomValueFromArray(array) {
   return array[random];
 }
 
-// Raw text strings
-
 const storyText = ['It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.']
 const insertX = ['Willy the Goblin, Big Daddy, Father Christmas']
 const insertY= ['the soup kitchen, Disneyland, the White House']
 const insertZ = ['spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and slithered away']
 
-
-
 // Completed result function
     function result(){
-        let newStory = storyText 
+        let newStory = storyText
+//Replaces placeholders with random select values
         const xItem = randomValueFromArray(insertX);
         const yItem = randomValueFromArray(insertY);
         const zItem = randomValueFromArray(insertZ);
+//Replaces all previous placeholders in story with new ones
+        newStory = newStory.replaceAll(':insertx:',xItem);
+        newStory = newStory.replaceAll(':inserty:',yItem);
+        newStory = newStory.replaceAll(':insertz:',zItem);
+        
     
   return storyText;
 }
