607896b0350adf88273e07283367059f276a55ed
Haris Hussain <haris_hussain@outlook.com>
2025-12-05 08:15:54 -0500

    Add thumbnail image loop and click handler
    in main.js. loops through the array of images starting at 1 (that is included in the file name of the first picture, not 0 the index of the first picture in the array)
    
    in main.js, adds an event listener for the "click" event on all thumbnails

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -13,3 +13,21 @@ const images = [
     { filename: 'pic4.jpg', alt: 'Section of wall from a pharaohs tomb'},
     { filename: 'pic5.jpg', alt: 'Large moth on a leaf' },
 ];
+
+//base URL
+const baseURL = 'https://haris0171.github.io/lab4/part2/images'
+
+//loops through the array of images starting at 1
+for (let i = 1; i <= images.length; i++) {
+    const newImage = document.createElement("img");
+    newImage.src = `${baseURL}pic${i}.jpg`;
+    newImage.alt = images[i - 1].alt; // match correct alt text
+    thumbBar.appendChild(newImage);
+
+//event lisnter
+    newImage.addEventListener("click", updateDisplayedImage);
+ }
+    function updateDisplayedImage(e) {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+}
