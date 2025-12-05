5204e798411e10ba831ef4c6ee7c533a3c667fbb
Haris Hussain <haris_hussain@outlook.com>
2025-12-05 02:22:06 -0500

    Initializing Variables and Functions
    created a storyText variable and insertX, insertY, and insertZ arrays from the provided files in main.js
    
    copied the provided event handler and incomplete function in main.js
    
    in result() function creates a new variable newStory set equal to storyText
    
    in result() function creates three (3) variables xitem, yItem and zItem that are respectively set by selecting a random element from insertX, insertY and insertZ using the randowmValueFromArray() function

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -11,23 +11,20 @@ function randomValueFromArray(array) {
 
 // Raw text strings
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
+const storyText = ['It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.']
+const insertX = ['Willy the Goblin, Big Daddy, Father Christmas']
+const insertY= ['the soup kitchen, Disneyland, the White House']
+const insertZ = ['spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and slithered away']
 
-// the soup kitchen
-// Disneyland
-// the White House
 
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
 
-// Partial return random string function
-
-function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-
+// Completed result function
+    function result(){
+        let newStory = storyText 
+        const xItem = randomValueFromArray(insertX);
+        const yItem = randomValueFromArray(insertY);
+        const zItem = randomValueFromArray(insertZ);
+    
   return storyText;
 }
 
